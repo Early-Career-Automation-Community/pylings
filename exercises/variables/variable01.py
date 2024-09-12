1f92d188c40dab66581c2d8a3974dd410dea9da1
Matheus Tanaka <matheustanakanog@gmail.com>
2024-09-12 18:28:51 -0300

    add first exercise

---
+++ exercises/variables/variable01.py
@@ -0,0 +1,19 @@
+# O que são variavéis no Python
+#
+
+# Como declarar uma variavel
+
+num = 10
+nome = 'Matheus'
+verdadeiro = False
+
+print("Trabalhando com números: ", num)
+print("Trabalhando com string: ", nome)
+print("Trabalhando com booleanos: ", verdadeiro)
+
+# Exercício - adicione os valores as variavéis
+
+num1 
+num2
+
+print("A soma dos dois números é: ", num1+num2)
